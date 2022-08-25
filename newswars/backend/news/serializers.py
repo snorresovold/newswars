4d2be9a89dbb9fdd0ad42e279fe42846e8011332
snorresovold <snorresovold@gmail.com>
2022-08-25 21:01:58 +0200

    deleting everything and rewriting it because of aws

--- newswars/backend/news/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import News
-
-
-class NewsSerializer(ModelSerializer):
-    class Meta:
-        model = News
-        fields = '__all__'
--- newswars/backend/news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import NewsViewSet
-
-urlpatterns = [
-    path("", NewsViewSet.as_view()),
-]
