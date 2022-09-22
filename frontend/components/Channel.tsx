53c324433c30adf7f1299b823f3768e68934f94b
snorresovold <snorresovold@gmail.com>
2022-09-22 21:48:04 +0200

    updates

--- frontend/components/Channel.tsx
+++ frontend/components/Channel.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Post from './Post'
-import Image from 'next/image'
 
 function Channel({ props }:any) {
   const name = props.name // looks ugly but works (wtf)
   const color = props.color
   return (
-    <div className='m-4 w-96 box-border border-4 overflow-y-scroll h-3/6'>
+    <div className='m-4 w-96 box-border border-4 overflow-y-scroll h-1/6'>
       <div>
         <img src={props.logo} width={1000} height={1000}/>
       </div>
