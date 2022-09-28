b48454bb0f67c615c07e0b6cb5c0ea13e58a215c
snorresovold <snorresovold@gmail.com>
2022-09-28 10:32:44 +0200

    turned the entire thing into a function

--- services/parse_xml.py
+++ services/parse_xml.py
@@ -1,50 +1,53 @@
 import requests
 import xml.etree.ElementTree as ET
 
-# url of rss feed
-url = "https://www.vg.no/rss/feed"
 
-# creating HTTP response object from given url
-resp = requests.get(url)
+def parse(url):
+    # url of rss feed
 
-# saving the xml file
-with open('topnewsfeed.xml', 'wb') as f:
-    f.write(resp.content)
+    # creating HTTP response object from given url
+    resp = requests.get(url)
 
-#print(resp.content)
-def parseXML(xmlfile):
-  
-    # create element tree object
-    tree = ET.parse(xmlfile)
-  
-    # get root element
-    root = tree.getroot()
-  
-    # create empty list for news items
-    newsitems = []
-  
-    # iterate news items
-    for item in root.findall('./channel/item'):
-  
-        # empty news dictionary
-        news = {}
-  
-        # iterate child elements of item
-        for child in item:
-  
-            # special checking for namespace object content:media
-            if child.tag == '{http://search.yahoo.com/mrss/}content':
-                news['media'] = child.attrib['url']
-            else:
-                news[child.tag] = str(child.text).encode('utf8')
-  
-        # append news dictionary to news items list
-        newsitems.append(news)
-      
-    # return news items list
-    #print(newsitems[0])
-    for x in newsitems:
-        print(x)
-    return newsitems
+    # saving the xml file
+    with open('topnewsfeed.xml', 'wb') as f:
+        f.write(resp.content)
 
-parseXML("topnewsfeed.xml")
+    #print(resp.content)
+    def parseXML(xmlfile):
+    
+        # create element tree object
+        tree = ET.parse(xmlfile)
+    
+        # get root element
+        root = tree.getroot()
+    
+        # create empty list for news items
+        newsitems = []
+    
+        # iterate news items
+        for item in root.findall('./channel/item'):
+    
+            # empty news dictionary
+            news = {}
+    
+            # iterate child elements of item
+            for child in item:
+    
+                # special checking for namespace object content:media
+                if child.tag == '{http://search.yahoo.com/mrss/}content':
+                    news['media'] = child.attrib['url']
+                else:
+                    news[child.tag] = str(child.text).encode('utf8')
+    
+            # append news dictionary to news items list
+            newsitems.append(news)
+        
+        # return news items list
+        #print(newsitems[0])
+        for x in newsitems:
+            print(x)
+        return newsitems
+
+    parseXML("topnewsfeed.xml")
+
+parse("https://www.dagbladet.no/?lab_viewport=rss")
