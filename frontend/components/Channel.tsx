1c1da97afad7db36651dfa1e856d22abfe658456
snorresovold <snorresovold@gmail.com>
2022-10-15 04:22:32 +0200

    updates

--- frontend/components/Channel.tsx
+++ frontend/components/Channel.tsx
@@ -5,7 +5,7 @@ function Channel({ props }:any) {
   const name = props.name // looks ugly but works (wtf)
   const color = props.color
   return (
-    <div className='grid grid-rows-4 m-4 w-96 box-border border-4 overflow-y-scroll h-96'>
+    <div className=' m-4 w-96 box-border border-4 overflow-y-scroll h-96'>
       <div>
       {props.news.map((props: any) =>
         <Post props={props} channel={name} color={color} />
